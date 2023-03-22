2bba0b3f837f73a3964cf0a05697112e52c2a5f6
Benjale <benjalamos@uc.cl>
2023-03-22 16:16:01 -0300

    add pipe 2.0

--- htmlConvertor.js
+++ htmlConvertor.js
@@ -1,6 +1,28 @@
 const pipe = functions => data => {
   return functions.reduce( (value, func) => func(value), data);
   };
+
+ConvertHeaders = (line) => 
+  line
+      .replace(/^#\s(.*)$/gm, '<h1>$1</h1>')
+      .replace(/^##\s(.*)$/gm, '<h2>$1</h2>')
+      .replace(/^###\s(.*)$/gm, '<h3>$1</h3>')
+      .replace(/^####\s(.*)$/gm, '<h4>$1</h4>')
+      .replace(/^#####\s(.*)$/gm, '<h5>$1</h5>')
+      .replace(/^######\s(.*)$/gm, '<h6>$1</h6>')
+
+ConvertTextTypes = (line) =>
+  line
+      .replace(/\*\*(.*)\*\*/gm, '<strong>$1</strong>')
+      .replace(/\*(.*)\*/gm, '<em>$1</em>')
+
+
+ConvertTextStructure = (prevLine, line) => 
+  line
+      .replace(/^\>\s(.*)$/gm, '<blockquote>$1</blockquote>')
+      .replace(/^\*\s(.*)$/gm, (prevLine.substring(0,2) !== '* ') ? '<ul>\n     <li>$1</li>' : '     <li>$1</li>')
+      .replace(/^(?!<| )(.*)$/gm, '<p>$1</p>')
+
   
 const markdownToHtml = (markdownText) => {
   let lines = markdownText.split("\n");
@@ -9,7 +31,7 @@ const markdownToHtml = (markdownText) => {
 
   lines.forEach(function(line){
 
-    // Determina si se acaba de terminar un punteo
+    // Determina si se acaba de terminar un punteo (ul)
     const ulFinished = pipe( [
       x => x.substring(0,2), 
       x => x != '* ', 
@@ -19,24 +41,13 @@ const markdownToHtml = (markdownText) => {
     if(ulFinished){
       response += '</ul>\n'
     }
-    
-    const htmlLine = line
-      // Replace #
-      .replace(/^#\s(.*)$/gm, '<h1>$1</h1>')
-      .replace(/^##\s(.*)$/gm, '<h2>$1</h2>')
-      .replace(/^###\s(.*)$/gm, '<h3>$1</h3>')
-      .replace(/^####\s(.*)$/gm, '<h4>$1</h4>')
-      .replace(/^#####\s(.*)$/gm, '<h5>$1</h5>')
-      .replace(/^######\s(.*)$/gm, '<h6>$1</h6>')
-      // Replace >
-      .replace(/^\>\s(.*)$/gm, '<blockquote>$1</blockquote>')
-      // Replace **
-      .replace(/\*\*(.*)\*\*/gm, '<strong>$1</strong>')
-      .replace(/\*(.*)\*/gm, '<em>$1</em>')
-      // Replace *
-      .replace(/^\*\s(.*)$/gm, (prevLine.substring(0,2) !== '* ') ? '<ul>\n     <li>$1</li>' : '     <li>$1</li>')
-      // Replace new paragraph
-      .replace(/^(?!<| )(.*)$/gm, '<p>$1</p>')
+
+    htmlLine = pipe([
+      x => ConvertHeaders(x),
+      x => ConvertTextTypes(x),
+      x => ConvertTextStructure(prevLine, x)
+    ])(line)
+
     response += `${htmlLine}\n`;
     prevLine = line;
   })

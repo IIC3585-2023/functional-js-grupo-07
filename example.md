7cde8baab5ca788db08d3320f3fb72da3cba7d7e
Juan Pablo Anwandter <janwandter@uc.cl>
2023-03-25 12:57:44 -0300

    feat: combinator

--- example.md
+++ example.md
@@ -1,7 +1,19 @@
-# Hello World
-This **is** a sample *paragraph*.
-* Bullet point 1
-* Bullet point 2
-* Bullet point 3
-## Subheading
-> This is a blockquote.
+# El formato Markdown
+
+## Parte 1
+
+El formato de markdown es ampliamente utilizado porque permite describir un **contenido en forma estructurada** en forma sencilla.
+
+Por ejemplo se puede usar en _GitHub_ para documentar el código, escribir manuales, etc.
+
+## Parte 2
+
+Markdown puede ser usado para muchas cosas. Por ejemplo la gente lo utiliza para
+
+* crear páginas web
+* publicar documentos,
+* escribir notas
+* hacer presentaciones
+* redactar correos
+* generar documentación técnica
+* escribir libros
--- htmlConvertor.js
+++ htmlConvertor.js
@@ -5,68 +5,96 @@ const readline = require('readline').createInterface({
 });
 var _ = require('lodash');
 
-const pipe = functions => data => {
-  return functions.reduce( (value, func) => func(value), data);
-  };
+const pipe = (functions) => (data) => {
+  return functions.reduce((value, func) => func(value), data);
+};
 
-ConvertHeaders = (line) => 
+const finishUL = (line, prevLine) => {
+  line =
+    line.substring(0, 2) != '* ' && prevLine.substring(0, 2) == '* '
+      ? '</ul>\n' + line
+      : line; // Determina si se acaba de terminar un punteo (ul)
+  return line;
+};
+
+const ConvertHeaders = (line) => {
+  line = S(headerConverter)(hashtagCounter)(line);
+  return line;
+};
+
+const S = (f) => (g) => (x) => {
+  return f(x)(g(x));
+};
+
+const headerConverter = (line) => (hashtagQuantity) => {
+  if (hashtagQuantity > 0) {
+    line = `<h${hashtagQuantity}>${line.substring(
+      hashtagQuantity + 1
+    )}</h${hashtagQuantity}>`;
+  }
+  return line;
+};
+
+const hashtagCounter = (line) => {
+  return line.split('#').length - 1;
+};
+
+ConvertTextTypes = (line) => {
+  return line
+    .replace(/\*\*(.*)\*\*/gm, '<strong>$1</strong>')
+    .replace(/\*(.*)\*/gm, '<em>$1</em>');
+};
+
+ConvertTextStructure = (prevLine, line) =>
   line
-      .replace(/^#\s(.*)$/gm, '<h1>$1</h1>')
-      .replace(/^##\s(.*)$/gm, '<h2>$1</h2>')
-      .replace(/^###\s(.*)$/gm, '<h3>$1</h3>')
-      .replace(/^####\s(.*)$/gm, '<h4>$1</h4>')
-      .replace(/^#####\s(.*)$/gm, '<h5>$1</h5>')
-      .replace(/^######\s(.*)$/gm, '<h6>$1</h6>')
+    .replace(/^\>\s(.*)$/gm, '<blockquote>$1</blockquote>')
+    .replace(
+      /^\*\s(.*)$/gm,
+      prevLine.substring(0, 2) !== '* '
+        ? '<ul>\n     <li>$1</li>'
+        : '     <li>$1</li>'
+    )
+    .replace(/^(?!<| )(.*)$/gm, '<p>$1</p>');
 
-ConvertTextTypes = (line) =>
-  line
-      .replace(/\*\*(.*)\*\*/gm, '<strong>$1</strong>')
-      .replace(/\*(.*)\*/gm, '<em>$1</em>')
-
-
-ConvertTextStructure = (prevLine, line) => 
-  line
-      .replace(/^\>\s(.*)$/gm, '<blockquote>$1</blockquote>')
-      .replace(/^\*\s(.*)$/gm, (prevLine.substring(0,2) !== '* ') ? '<ul>\n     <li>$1</li>' : '     <li>$1</li>')
-      .replace(/^(?!<| )(.*)$/gm, '<p>$1</p>')
-
-  
 const markdownToHtml = (markdownText) => {
-  let lines = markdownText.split("\n");
+  let lines = markdownText.split('\n');
   let prevLine = '';
 
-  let htmlLines = _.reduce(lines, function (result, line) {
+  let htmlLines = _.reduce(
+    lines,
+    function (result, line) {
+      result += pipe([
+        (x) => ConvertHeaders(x),
+        (x) => finishUL(x, prevLine),
+        (x) => ConvertTextTypes(x),
+        (x) => ConvertTextStructure(prevLine, x),
+        (x) => `${x}\n`,
+      ])(line);
 
-    result +=  pipe([
-                  x => x = (x.substring(0,2) != '* ' && prevLine.substring(0,2) == '* ')? '</ul>\n' + x: x, // Determina si se acaba de terminar un punteo (ul)
-                  x => ConvertHeaders(x),
-                  x => ConvertTextTypes(x),
-                  x => ConvertTextStructure(prevLine, x),
-                  x => `${x}\n`
-                ])(line)
-                
+      prevLine = line;
 
-    prevLine = line;
-    
-    return result;
-  }, '');
+      return result;
+    },
+    ''
+  );
 
-  return htmlLines
-}
+  return htmlLines;
+};
 
-
-
-readline.question('Name of the file to convert (without extension): ', fileName => {
-  if (fileName == "") {
-    fileName = 'example';
-  }
-  fs.readFile(`${fileName}.md`, 'utf-8', (err, data) => {
-    if (err) throw err;
-    const html = markdownToHtml(data);
-    fs.writeFile('output.html', html, (err) => {
+readline.question(
+  'Name of the file to convert (without extension): ',
+  (fileName) => {
+    if (fileName == '') {
+      fileName = 'example';
+    }
+    fs.readFile(`${fileName}.md`, 'utf-8', (err, data) => {
       if (err) throw err;
-      console.log('The file has been saved!');
+      const html = markdownToHtml(data);
+      fs.writeFile('output.html', html, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
     });
-  })
-  readline.close();
-});
+    readline.close();
+  }
+);
